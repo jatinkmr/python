bb8f27e98b987c6ce84ea24d1f3f2e329c3320c6
Jatin Kamboj <kumar.jatin873@gmail.com>
2025-06-23 11:46:32 +0530

    lecture1

---
+++ lecture1/variablesAndDataTypes.py
@@ -0,0 +1,51 @@
+print('hello');
+
+name="jatinkmr";
+print('name -> ', name);
+print('type of name -> ', type(name))
+
+name1='jatinkmr';
+print('name1 -> ', name1);
+print('type of name1 -> ', type(name1))
+
+name2='''jatinkmr''';
+print('name2 -> ', name2);
+print('type of name2 -> ', type(name2))
+
+age=38
+print('age -> ', age);
+print('type of age -> ', type(age))
+
+price=25.96
+print('price -> ', price);
+print('type of price -> ', type(price))
+
+boolValue=True
+print('boolValue -> ', boolValue);
+print('type of boolValue -> ', type(boolValue))
+
+boolValue1=False
+print('boolValue1 -> ', boolValue1);
+print('type of boolValue1 -> ', type(boolValue1))
+
+# single line comment
+'''
+multi line
+comment
+'''
+
+a=int(input('Enter value of a')) # default input will be string
+b=2
+print('sum', a+b)
+print('diff', a-b)
+print('mul', a*b)
+print('div', a/b)
+print('modulus', a%b)
+print('Exponentiation', a**b)
+txt=f"Floor division {a//b}"
+print(txt)
+
+float1=float(input('Enter value of float1...'))
+float2=float(input('Enter value of float2...'))
+print((float1+float2)/2);
+
